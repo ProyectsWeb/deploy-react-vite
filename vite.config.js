b3f5dd0b23470a75fa9e5fc141db051b58c2bce9
Jorge Peraza <jorgeph2013@gmail.com>
2023-02-20 22:53:57 -0800

    añadiendo el comentario del video explicacion para el deploy en github pages

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: "/deploy-react-vite/"
 })
+
+//VIDEO DONDE SE EXPLICA CORRECTAMENTE EL DEPLOY DE REAC A GITHUB PAGES
+// https://www.youtube.com/watch?v=igCO5i4NnfU
